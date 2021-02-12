0a7d8a4999fbb7833c9f29ac722d2251b27051e4
mairambek <70689476+usonmairambek@users.noreply.github.com>
2021-02-12 15:01:57 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ burger.onclick = function () {
 }
 
 
+$(function() {
+    $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+    $('#topNubex').fadeIn();
+    } else {
+    $('#topNubex').fadeOut();
+    }
+    });
+    $('#topNubex').click(function() {
+    $('body,html').animate({scrollTop:0},700);
+    });
+    });
