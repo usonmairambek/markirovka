5b13d368553b11ea9a595efdef93786c26e7c9fa
mairambek <70689476+usonmairambek@users.noreply.github.com>
2021-02-12 14:39:41 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var burger = document.getElementById('burger');
+// var test = document.getElementById('test')
+// burger.onclick = function (){
+// burger.classList.toggle("active");
+// }
+
+
+
+burger.onclick = function () {
+    burger.classList.toggle("active");
+    test.classList.toggle("tests");
+}
+
+
